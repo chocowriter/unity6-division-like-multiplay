6f5070446fa91d16b36c273c765099a82ede931c
chocowriter <chocowriter0@gmail.com>
2026-01-19 09:19:54 +0900

    add power console asset

---
+++ Assets/Common/Scripts/SceneController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DivisionLike
+{
+    public enum SceneName
+    {
+        MainMenu,
+        SinglePlay,
+        MultiPlay,
+        Training
+    }
+    
+    public class SceneController : MonoBehaviour
+    {
+        
+    }
+}
+
+
