96e1d692b5dcd4d670b5b0b49298481c2b1b9782
chocowriter <chocowriter0@gmail.com>
2026-01-23 14:01:05 +0900

    new input system

--- Assets/UI/Scripts/Common/Tooltip.cs
+++ Assets/UI/Scripts/Common/Tooltip.cs
@@ -1,10 +1,22 @@
+using System;
 using UnityEngine;
 using WanzyeeStudio;
+using TMPro;
 
 namespace DivsionLike
 {
     public class Tooltip : BaseSingleton<Tooltip>
     {
-    
+        [SerializeField] private TextMeshProUGUI m_Text;
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        private void Update()
+        {
+            
+        }
     }    
 }
